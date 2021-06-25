e78298282e9542cb39baf12cbfd96524b007b4e9
dilli <dilli@DILLI-K.local>
2021-06-26 00:10:46 +0530

    Setting up for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('click', (event)=> {
 
     messageOne.textContent = 'Loading...!';
     messageTwo.textContent = '';
-    fetch('http://localhost:3000/weather?address='+location).then(
+    fetch('/weather?address='+location).then(
         (response) => {
             response.json().then((data) => {
                 if(data.error){
